aa9a8ba64ea85433a049f145a31416b03a2365f5
ashankaushalya97 <ashankaushalya952@gmail.com>
2020-01-07 23:14:14 +0530

    Edit mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,11 +1,14 @@
-#ifndef MAINWINDOW_H
+//#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QList>
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+//QT_BEGIN_NAMESPACE
+//namespace Ui { class MainWindow; }
+//QT_END_NAMESPACE
+
+class scribblearea;
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +18,37 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+        void closEvent(QCloseEvent *event) override;
+
+private slots:
+        void opem();
+        void save();
+        void penColor();
+        void penWidth();
+        void about();
+
 private:
-    Ui::MainWindow *ui;
+//    Ui::MainWindow *ui;
+    void createActions();
+    void createMenus();
+    bool maybeSave();
+    bool saveFile(const QByteArray &fileFormate);
+    scribblearea *scribbleArea;
+    QMenu *saveAsMenu;
+    QMenu *fileMenu;
+    QMenu *optionMenu;
+    QMenu *helpMenu;
+    QAction *openAct;
+    QList<QAction *> saveAsActs;
+    QAction *penColorAct;
+    QAction *penWidthAct;
+    QAction *printAct;
+    QAction *clearScreenAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+
+
+
 };
-#endif // MAINWINDOW_H
+//#endif // MAINWINDOW_H
