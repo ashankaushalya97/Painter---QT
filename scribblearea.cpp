d4bda780556c0985fc5408748521a901860f9d2b
ashankaushalya97 <ashankaushalya952@gmail.com>
2020-01-08 19:01:42 +0530

    Edit scribblearea.cpp

--- scribblearea.cpp
+++ scribblearea.cpp
@@ -67,6 +67,97 @@ void ScribbleArea::mouseReleaseEvent(QMouseEvent *event){
     }
 
 }
+void ScribbleArea::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+
+    // Returns the rectangle that needs to be updated
+    QRect dirtyRect = event->rect();
+
+    // Draws the rectangle where the image needs to
+    // be updated
+    painter.drawImage(dirtyRect, image, dirtyRect);
+}
+
+// Resize the image to slightly larger then the main window
+// to cut down on the need to resize the image
+void ScribbleArea::resizeEvent(QResizeEvent *event)
+{
+    if (width() > image.width() || height() > image.height()) {
+        int newWidth = qMax(width() + 128, image.width());
+        int newHeight = qMax(height() + 128, image.height());
+        resizeImage(&image, QSize(newWidth, newHeight));
+        update();
+    }
+    QWidget::resizeEvent(event);
+}
+
+void ScribbleArea::drawLineTo(const QPoint &endPoint)
+{
+    // Used to draw on the widget
+    QPainter painter(&image);
+
+    // Set the current settings for the pen
+    painter.setPen(QPen(myPenColor, myPenWidth, Qt::SolidLine, Qt::RoundCap,
+                        Qt::RoundJoin));
+
+    // Draw a line from the last registered point to the current
+    painter.drawLine(lastPoint, endPoint);
+
+    // Set that the image hasn't been saved
+    modified = true;
+
+    int rad = (myPenWidth / 2) + 2;
+
+    // Call to update the rectangular space where we drew
+    update(QRect(lastPoint, endPoint).normalized()
+                                     .adjusted(-rad, -rad, +rad, +rad));
+
+    // Update the last position where we left off drawing
+    lastPoint = endPoint;
+}
+
+// When the app is resized create a new image using
+// the changes made to the image
+void ScribbleArea::resizeImage(QImage *image, const QSize &newSize)
+{
+    // Check if we need to redraw the image
+    if (image->size() == newSize)
+        return;
+
+    // Create a new image to display and fill it with white
+    QImage newImage(newSize, QImage::Format_RGB32);
+    newImage.fill(qRgb(255, 255, 255));
+
+    // Draw the image
+    QPainter painter(&newImage);
+    painter.drawImage(QPoint(0, 0), *image);
+    *image = newImage;
+}
+
+// Print the image
+void ScribbleArea::print()
+{
+    // Check for print dialog availability
+#if QT_CONFIG(printdialog)
+
+    // Can be used to print
+    QPrinter printer(QPrinter::HighResolution);
+
+    // Open printer dialog and print if asked
+    QPrintDialog printDialog(&printer, this);
+    if (printDialog.exec() == QDialog::Accepted) {
+        QPainter painter(&printer);
+        QRect rect = painter.viewport();
+        QSize size = image.size();
+        size.scale(rect.size(), Qt::KeepAspectRatio);
+        painter.setViewport(rect.x(), rect.y(), size.width(), size.height());
+        painter.setWindow(image.rect());
+        painter.drawImage(0, 0, image);
+    }
+#endif // QT_CONFIG(printdialog)
+}
+
 
 
 
